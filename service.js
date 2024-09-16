9ed1c1f8d1a8edae5a8262f48fd79f66aefbad22
MuzamilMohammadK <muzamilmohammadk@gmail.com>
2024-09-16 10:58:10 +0530

    Create service.js

---
+++ service.js
@@ -0,0 +1,66 @@
+const ServiceComponent: React.VFC = () => {
+  return (
+    <Service height="700px" width="1300px">
+      <buttons flexDirection="row" justifyContent="center" alignItems="center" flex="1" style={{paddingTop:10,paddingBottom:10,paddingLeft:10,paddingRight:10}}>
+        <HomeText>Home</HomeText>
+      </buttons>
+      <buttons flexDirection="row" justifyContent="center" alignItems="center" flex="1" style={{paddingTop:10,paddingBottom:10,paddingLeft:10,paddingRight:10}}>
+        <HomeText>About</HomeText>
+      </buttons>
+      <buttons flexDirection="row" justifyContent="center" alignItems="center" flex="1" style={{paddingTop:10,paddingBottom:10,paddingLeft:10,paddingRight:10}}>
+        <HomeText>Service</HomeText>
+      </buttons>
+      <buttons flexDirection="row" justifyContent="center" alignItems="center" flex="1" style={{paddingTop:10,paddingBottom:10,paddingLeft:10,paddingRight:10}}>
+        <HomeText>skills</HomeText>
+      </buttons>
+      <buttons flexDirection="row" justifyContent="center" alignItems="center" flex="1" style={{paddingTop:10,paddingBottom:10,paddingLeft:10,paddingRight:10}}>
+        <HomeText>Projects</HomeText>
+      </buttons>
+      <buttons flexDirection="row" justifyContent="center" alignItems="center" flex="1" style={{paddingTop:10,paddingBottom:10,paddingLeft:10,paddingRight:10}}>
+        <HomeText>Contact</HomeText>
+      </buttons>
+      <buttons flexDirection="row" justifyContent="center" alignItems="center" flex="1" style={{paddingTop:10,paddingBottom:10,paddingLeft:10,paddingRight:10}}>
+        <HomeText>Service.</HomeText>
+      </buttons>
+      <Rectangle14 />
+      <about flexDirection="row" justifyContent="center" alignItems="center" flex="1" style={{paddingTop:10,paddingBottom:10,paddingLeft:10,paddingRight:10}}>
+        <</>Text></></</>Text>
+      </about>
+      <about flexDirection="row" justifyContent="center" alignItems="center" flex="1" style={{paddingTop:10,paddingBottom:10,paddingLeft:10,paddingRight:10}}>
+        <UI/UXDesignText>UI/UX Design</UI/UXDesignText>
+      </about>
+      <About flexDirection="row" justifyContent="center" alignItems="center" flex="1" style={{paddingTop:10,paddingBottom:10,paddingLeft:10,paddingRight:10}}>
+        <Ispecializeincraftingintuitiveandvisuallyappealinguserexperiences.Byblendinguserresearchanddesignthinking,Ideliversolutionsthatarebothfunctionalandengaging.Mygoalistobringyourvisiontolifewithdesignsthatresonateanddelight.Text>          I specialize in crafting intuitive and visually appealing user experiences. By blending user research and design thinking, I deliver solutions that are both functional and engaging. My goal is to bring your vision to life with designs that resonate and delight.</Ispecializeincraftingintuitiveandvisuallyappealinguserexperiences.Byblendinguserresearchanddesignthinking,Ideliversolutionsthatarebothfunctionalandengaging.Mygoalistobringyourvisiontolifewithdesignsthatresonateanddelight.Text>
+      </About>
+      <learnmore flexDirection="row" justifyContent="center" alignItems="center" flex="1" style={{paddingTop:10,paddingBottom:10,paddingLeft:10,paddingRight:10}}>
+        <LearnmoreText>Learn more</LearnmoreText>
+      </learnmore>
+      <Rectangle15 />
+      <about flexDirection="row" justifyContent="center" alignItems="center" flex="1" style={{paddingTop:10,paddingBottom:10,paddingLeft:10,paddingRight:10}}>
+        <DataVisualizationText>Data Visualization</DataVisualizationText>
+      </about>
+      <About flexDirection="row" justifyContent="center" alignItems="center" flex="1" style={{paddingTop:10,paddingBottom:10,paddingLeft:10,paddingRight:10}}>
+        <Datavisualizationasaservice(DVaaS)providescustomizable,cloud-basedtoolstocreateandmanagevisualdatainsights.Itenablesbusinessestoefficientlytransformcomplexdataintointeractivecharts,graphs,anddashboards.WithDVaaS,organizationscanmakedata-drivendecisionsfasterandmoreeffectivelywithouttheneedforin-houseexpertise.Text>          Data visualization as a service (DVaaS) provides customizable, cloud-based tools to create and manage visual data insights. It enables businesses to efficiently transform complex data into interactive charts, graphs, and dashboards. With DVaaS, organizations can make data-driven decisions faster and more effectively without the need for in-house expertise.</Datavisualizationasaservice(DVaaS)providescustomizable,cloud-basedtoolstocreateandmanagevisualdatainsights.Itenablesbusinessestoefficientlytransformcomplexdataintointeractivecharts,graphs,anddashboards.WithDVaaS,organizationscanmakedata-drivendecisionsfasterandmoreeffectivelywithouttheneedforin-houseexpertise.Text>
+      </About>
+      <learnmore flexDirection="row" justifyContent="center" alignItems="center" flex="1" style={{paddingTop:10,paddingBottom:10,paddingLeft:10,paddingRight:10}}>
+        <LearnmoreText>Learn more</LearnmoreText>
+      </learnmore>
+      <VectorImage />
+    </Service>
+  )
+}
+
+const Service = createBox<Theme>();
+const buttons = createBox<Theme>();
+const HomeText = createText<Theme>();
+const Rectangle14 = createBox<Theme>();
+const about = createBox<Theme>();
+const </>Text = createText<Theme>();
+const UI/UXDesignText = createText<Theme>();
+const About = createBox<Theme>();
+const Ispecializeincraftingintuitiveandvisuallyappealinguserexperiences.Byblendinguserresearchanddesignthinking,Ideliversolutionsthatarebothfunctionalandengaging.Mygoalistobringyourvisiontolifewithdesignsthatresonateanddelight.Text = createText<Theme>();
+const learnmore = createBox<Theme>();
+const LearnmoreText = createText<Theme>();
+const Rectangle15 = createBox<Theme>();
+const DataVisualizationText = createText<Theme>();
+const Datavisualizationasaservice(DVaaS)providescustomizable,cloud-basedtoolstocreateandmanagevisualdatainsights.Itenablesbusinessestoefficientlytransformcomplexdataintointeractivecharts,graphs,anddashboards.WithDVaaS,organizationscanmakedata-drivendecisionsfasterandmoreeffectivelywithouttheneedforin-houseexpertise.Text = createText<Theme>();
